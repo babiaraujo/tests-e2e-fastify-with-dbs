19d0378aa580e31bb15d46ff6d4eb45702951d57
Barbara Araujo <barbaraaraujo@MacBook-Air-de-Barbara.local>
2024-03-18 21:14:38 -0300

    chore: adding logic of users and seed

---
+++ config/users.js
@@ -0,0 +1,15 @@
+const users = [
+    {
+        name: 'Babi Araujo',
+        phone: '12936180786',
+    },
+    {
+        name: 'Tarcisio Philips',
+        phone: '12987654321',
+    },
+    {
+        name: 'Ana Julia Candia',
+        phone: '11123456789',
+    }
+]
+export { users }
---
+++ test/api.test.js
@@ -0,0 +1,101 @@
+import { describe, beforeEach, before, after, it } from 'node:test'
+import { deepStrictEqual } from 'node:assert'
+import { runSeed } from '../config/seed.js'
+import { users } from '../config/users.js'
+
+describe('API Workflow', () => {
+    let _testServer
+    let _testServerAddress
+
+    function createCustomer(customer) {
+        return _testServer.inject({
+            method: 'POST',
+            url: `${_testServerAddress}/customers`,
+            payload: customer,
+        })
+    }
+
+    function getCustomers() {
+        return _testServer.inject({
+            method: 'GET',
+            url: `${_testServerAddress}/customers`,
+        })
+    }
+
+    async function validateUsersListOrderedByName(usersSent) {
+        const res = await getCustomers()
+        const statusCode = res.statusCode
+        const result = await res.json()
+        const expectSortedByName = usersSent.sort((a, b) => a.name.localeCompare(b.name))
+        deepStrictEqual(statusCode, 200)
+        deepStrictEqual(result, expectSortedByName)
+    }
+
+    before(async () => {
+        const { server } = await import('../src/index.js')
+        _testServer = server
+
+        _testServerAddress = await server.listen();
+    })
+
+    beforeEach(async () => {
+        return runSeed()
+    })
+
+    after(async () => _testServer.close())
+
+    describe('POST /customers', () => {
+        it('should create customer', async (context) => {
+
+            const input = {
+                name: 'Senhor Pink Floyd',
+                phone: '12123456789',
+            }
+
+            const expected = `user ${input.name} created!`
+
+            const { body } = await createCustomer(input)
+            deepStrictEqual(body, expected)
+
+            await validateUsersListOrderedByName([...users, input])
+        })
+    })
+
+    describe(`GET /customers`, () => {
+        it('should retrieve only initial users', async () => {
+            return validateUsersListOrderedByName(users)
+        })
+
+        it('given 5 different customers it should have valid list', async (context) => {
+
+            const customers = [
+                {
+                    name: 'Itachi Uchiha',
+                    phone: '123456789',
+                },
+                {
+                    name: 'Naruto Uzumaki',
+                    phone: '123456789',
+                },
+                {
+                    name: 'Mashle',
+                    phone: '123456789',
+                },
+                {
+                    name: 'Pokemon',
+                    phone: '123456789',
+                },
+                {
+                    name: 'Sasuke Uchiha',
+                    phone: '123456789',
+                },
+
+            ]
+            await Promise.all(
+                customers.map(customer => createCustomer(customer))
+            )
+
+            await validateUsersListOrderedByName(users.concat(customers))
+        })
+    })
+})
